29635c156c14ac1d5a9396f61ab45809373c5d6d
Denis Biryukov <cactusoid@gmail.com>
2020-08-13 14:10:45 +0300

    check

--- lesson29.py
+++ lesson29.py
@@ -5,7 +5,11 @@ True или False, соответственно.
 """
 
 def odd_ball(arr):
-    pass
+    for i in list(arr):
+        k = arr.index('odd')
+        if i == k:
+            return True
+    return False
 
 
 print(odd_ball(['even', 4, 'even', 7, 'even', 55, 'even', 6, 'even', 10, 'odd', 3, 'even']))  # True
@@ -20,7 +24,7 @@ print(odd_ball(['even', 10, 'odd', 2, 'even']))  # True
 '''
 
 def find_sum(n):
-    pass
+    print(sum([i for i in range(n + 1) if i % 3 == 0 or i % 5 == 0]))
 
 
 find_sum(5)
@@ -34,4 +38,9 @@ names = ['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul']
 '''
 
 def get_names(names):
-    pass
+    for i in range(len(names)):
+        if len(names[i]) == 4:
+            names1.append(names[i])
+    print(names1)
+names1 =[]
+get_names(['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul'])
