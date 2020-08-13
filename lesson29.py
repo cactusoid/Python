05066a7733a5429bcc435896bb4372b969ebc3c2
Denis Biryukov <cactusoid@gmail.com>
2020-08-13 14:42:02 +0300

    После просмотра решения домашнего задания

--- lesson29.py
+++ lesson29.py
@@ -4,13 +4,17 @@
 True или False, соответственно.
 """
 
-def odd_ball(arr):
-    for i in list(arr):
-        k = arr.index('odd')
-        if i == k:
-            return True
-    return False
+# Мой код
+# def odd_ball(arr):
+#     for i in list(arr):
+#         k = arr.index('odd')
+#         if i == k:
+#             return True
+#     return False
 
+# Решение от преподавателя
+def odd_ball(arr):
+    return arr.index('odd') in arr
 
 print(odd_ball(['even', 4, 'even', 7, 'even', 55, 'even', 6, 'even', 10, 'odd', 3, 'even']))  # True
 print(odd_ball(['even', 4, 'even', 7, 'even', 55, 'even', 6, 'even', 9, 'odd', 3, 'even']))  # False
@@ -23,13 +27,29 @@ print(odd_ball(['even', 10, 'odd', 2, 'even']))  # True
 способами (один из способов в одну строчку кода). 
 '''
 
+# мой код
+# def find_sum(n):
+#     print(sum([i for i in range(n + 1) if i % 3 == 0 or i % 5 == 0]))
+
+# find_sum(5)
+# find_sum(10)
+
+# Код преподавателя
 def find_sum(n):
-    print(sum([i for i in range(n + 1) if i % 3 == 0 or i % 5 == 0]))
+    res = 0
+    for i in range(n + 1):
+        if i % 3 == 0 or i % 5 == 0:
+            res += i
+    return res
 
+def find_sum2(n):
+    return sum(i for i in range(n + 1) if i % 3 == 0 or i % 5 == 0)
 
-find_sum(5)
-find_sum(10)
+print(find_sum(5))
+print(find_sum(10))
 
+print(find_sum2(5))
+print(find_sum2(10))
 
 
 '''
@@ -37,10 +57,19 @@ find_sum(10)
 names = ['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul']
 '''
 
+# Мой код
+# def get_names(names):
+#     for i in range(len(names)):
+#         if len(names[i]) == 4:
+#             names1.append(names[i])
+#     print(names1)
+# names1 =[]
+# get_names(['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul'])
+
+
+# Код преподавателя
+names = ['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul']
 def get_names(names):
-    for i in range(len(names)):
-        if len(names[i]) == 4:
-            names1.append(names[i])
-    print(names1)
-names1 =[]
-get_names(['Ryan', 'Kieran', 'Mark', 'John', 'David', 'Paul'])
+    return [i for i in names if len(i) == 4]
+
+print(get_names(names))
