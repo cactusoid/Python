f39753e1b0a3f6adc189a9a7c7865c4030fa73b0
Denis Biryukov <cactusoid@gmail.com>
2020-08-28 13:34:53 +0300

    lambda

---
+++ lesson43.py
@@ -0,0 +1,19 @@
+# def get_square(num):
+#     return num ** 2
+#
+# print(get_square(5))
+
+# get_square = lambda num: num ** 2
+# print(get_square(10))
+
+# print((lambda num: num ** 2)(15))
+
+l = [1, 2, 3]  #[2, 4, 6]
+
+# def get_double(l):
+#     return [i * 2 for i in l]
+
+def get_double(l):
+    return list(map(lambda num: num * 2, l))
+
+print(get_double(l))
